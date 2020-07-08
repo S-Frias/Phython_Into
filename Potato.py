ca29c242b2071b2fc30b5d96ee377a3146e2da21
VXIDA <68035651+VXIDA@users.noreply.github.com>
2020-07-08 19:12:24 -0400

    Add files via upload

---
+++ Potato.py
@@ -0,0 +1,43 @@
+print("Hello tiny Potatoes")
+print(" ")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+print(" ")
+print("Potatoes are delicious.")
+print("potatoes are very, very useful for many things")
+
+print(" ")
+Character_name = "Avatar"
+character_element = "Air"
+print("Water. Earth. Fire. " + character_element + ". My grandmother used to tell me stories about the old days: a time of peace when the " + Character_name + " kept balance between the Water Tribes, Earth Kingdom, Fire Nation and " + character_element + " Nomads. But that all changed when the Fire Nation attacked.")
+print("Only the " + Character_name + " mastered all four elements; only he could stop the ruthless firebenders. But when the world needed him most, he vanished. A hundred years have passed, and the Fire Nation is nearing victory in the war.")
+print("Two years ago, my father and the men of my tribe journeyed to the Earth Kingdom to help fight against the Fire Nation, leaving me and my brother to look after our tribe.")
+print("Some people believe that the " + Character_name + " was never reborn into the " + character_element + " Nomads and that the cycle is broken, but I haven't lost hope. I still believe that, somehow, the " + Character_name + " will return to save the world.")
+
+print(" ")
+character_name= "Tim"
+character_age= 510
+years = "500"
+print(" Once upon a time a child named " + character_name + " had been met with an angel.")
+print(" " +character_name + " had been given one wish, and little " + character_name + " had wanted to live forever.")
+print(" " + years + " years had passed and he is now",character_age ,".")
+
+print(" ")
+print("\"Water. Earth. Fire. " + character_element + ". My grandmother used to tell me stories about the old days: a time of peace when the " + Character_name + " kept balance between the Water Tribes, Earth Kingdom, Fire Nation and " + character_element + " Nomads. But that all changed when the Fire Nation attacked.\nOnly the " + Character_name + " mastered all four elements; only he could stop the ruthless firebenders. But when the world needed him most, he vanished. A hundred years have passed, and the Fire Nation is nearing victory in the war.\" ")
+
+print(" ")
+phrase = "potatoes"
+print(phrase + " ARE GOOD!!!!!!!")
+
+print(" ")
+print(phrase.lower() + " are good.")
+
+print(" ")
+print(phrase.upper() + " ARE GOOD!")
+
+print(" ")
+phrase = "oaighsfjcgbjdbksjgcvjkcbxvkjdgakjgdxkjvbakj"
+print(phrase + " was his name. ")
